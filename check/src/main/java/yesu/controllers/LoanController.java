15f77fd33b2a6e5b69db2ac670e4b0e09d6549cd
yesu <119784057+yesubabu7@users.noreply.github.com>
2023-09-07 11:09:06 +0530

    Update LoanController.java

--- check/src/main/java/yesu/controllers/LoanController.java
+++ check/src/main/java/yesu/controllers/LoanController.java
@@ -2,10 +2,11 @@ package yesu.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import yesu.entities.CustomerEntity;
 import yesu.models.LoanApplicationForm;
 import yesu.services.LoanServices;
 
@@ -20,23 +21,23 @@ public class LoanController {
 
 	}
 
-	
+	// Handle the POST request from the form submission
+	@RequestMapping(value = "/loansubmit", method = RequestMethod.POST) // Replace with your actual mapping
+	public String submitLoanApplication(@ModelAttribute("loanApplicationForm") LoanApplicationForm loanApplicationForm,
+			Model model) {
+		// You can perform further processing here, such as saving the form data to a database
+
+		// For demonstration purposes, we'll simply return a confirmation view with the submitted data
+		model.addAttribute("loanApplicationForm", loanApplicationForm);
+		System.out.println("success");
+
+		return "y"; // You should create a "confirmation.html" view for this
+	}
 
 	@RequestMapping(value = "/yesu", method = RequestMethod.GET)
 	public String getApplicationForm() {
 		System.out.println("");
 		return "LoanData";
 	}
-	
-	
-	@RequestMapping(value = "/babu", method = RequestMethod.POST)
-	public String getApplicationFor(LoanApplicationForm loanObj) {
-		System.out.println("hii"+loanObj.getFirstname());
-		
-		return "yes";
-		
-		
-	}
-	
 
 }
